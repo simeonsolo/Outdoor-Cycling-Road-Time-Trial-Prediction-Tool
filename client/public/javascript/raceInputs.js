12f716ae3703ec34ef7b6a7ff5bda970265c83dc
Simeon Solomou <simeon.solomou@student.adelaide.edu.au>
2022-10-25 04:46:19 +1030

    Updated raceInput.html to retrieve & send info to prediction model.

--- client/public/javascript/raceInputs.js
+++ client/public/javascript/raceInputs.js
@@ -41,9 +41,9 @@ function storeRaceInput() {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            /* success */
-            alert("Success");
-            // code to solve prediction model
+            /* call prediction model function */
+            let race = JSON.parse(this.response);
+            predictionModel(race);
         }
         else if (this.readyState == 4 && this.status >= 400 && this.status < 500) {
             /* fail */
@@ -51,10 +51,10 @@ function storeRaceInput() {
         }
         else if (this.readyState == 4 && this.status >= 500) {
             /* server error */
-            alert("Server Error"); /* alert */
+            alert("A race with this name already exists. Please enter a new name and try again."); /* alert */
         }
     };
     xhttp.open("POST", "/users/storeRaceInput", true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send(JSON.stringify(race));
-  }
+}
